dccc62c7c435048937949bb79777997438d9cb0f
SamAdebisi <samadebisi12@gmail.com>
2023-01-04 21:03:10 +0100

    setup

---
+++ polls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from .views import polls_list, polls_detail
+
+urlpatterns = [
+    path('polls/', polls_list, name="polls_list"),
+    path('polls/<int:pk>/', polls_detail, name="polls_detail"),
+]
