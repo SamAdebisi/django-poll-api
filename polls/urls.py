5e131332f7324a584476823eb2eabbbe598c24de
SamAdebisi <samadebisi12@gmail.com>
2023-01-06 12:59:35 +0100

    APIVIEW

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 
-from .views import polls_list, polls_detail
+# from .views import polls_list, polls_detail
+from .apiviews import PollList, PollDetail
 
 urlpatterns = [
-    path('polls/', polls_list, name="polls_list"),
-    path('polls/<int:pk>/', polls_detail, name="polls_detail"),
+    # path('polls/', polls_list, name="polls_list"),
+    path('polls/', PollList.as_view(), name="polls_list"),
+
+    # path('polls/<int:pk>/', polls_detail, name="polls_detail"),
+    path('polls/<int:pk>/', PollDetail.as_view(), name="polls_detail"),
 ]
