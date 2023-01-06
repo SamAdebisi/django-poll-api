8bc2f7306372be6b90502c364026728831de298a
SamAdebisi <samadebisi12@gmail.com>
2023-01-06 15:40:31 +0100

    DRF generic views

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,20 +1,45 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from .models import Poll, Choice
-from .serializers import PollSerializer
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
 
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-        return Response(data)
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(poll).data
-        return Response(data)
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(generics.ListCreateAPIView):
+    serializer_class = VoteSerializer
+
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from django.shortcuts import get_object_or_404
+#
+# from .models import Poll, Choice
+# from .serializers import PollSerializer
+#
+#
+# class PollList(APIView):
+#     def get(self, request):
+#         polls = Poll.objects.all()[:20]
+#         data = PollSerializer(polls, many=True).data
+#         return Response(data)
+#
+#
+# class PollDetail(APIView):
+#     def get(self, request, pk):
+#         poll = get_object_or_404(Poll, pk=pk)
+#         data = PollSerializer(poll).data
+#         return Response(data)
--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 # from .views import polls_list, polls_detail
-from .apiviews import PollList, PollDetail
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 
 urlpatterns = [
     # path('polls/', polls_list, name="polls_list"),
@@ -9,4 +9,7 @@ urlpatterns = [
 
     # path('polls/<int:pk>/', polls_detail, name="polls_detail"),
     path('polls/<int:pk>/', PollDetail.as_view(), name="polls_detail"),
+
+    path('choices/', ChoiceList.as_view(), name="choice_list"),
+    path('vote/', CreateVote.as_view(), name="create_vote"),
 ]
