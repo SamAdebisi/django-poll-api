3531630f1136f48058bc3e69ab00a599c341d58d
SamAdebisi <samadebisi12@gmail.com>
2023-01-16 12:57:02 +0100

    viewsets and routers

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,7 +1,19 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from rest_framework import viewsets
 
 from .models import Poll, Choice
-from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from .serializers import (
+    PollSerializer, ChoiceSerializer, VoteSerializer,
+)
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
 class PollList(generics.ListCreateAPIView):
@@ -15,12 +27,24 @@ class PollDetail(generics.RetrieveDestroyAPIView):
 
 
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
+        return queryset
     serializer_class = ChoiceSerializer
 
 
-class CreateVote(generics.ListCreateAPIView):
-    serializer_class = VoteSerializer
+class CreateVote(APIView):
+    # serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # from rest_framework.views import APIView
--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,28 @@
 from django.urls import path, include
 
-# from .views import polls_list, polls_detail
-from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from .apiviews import (
+    PollList, PollDetail,
+    ChoiceList, CreateVote,
+)
+from rest_framework.routers import DefaultRouter
+from .apiviews import PollViewSet
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, basename='polls')
 
 urlpatterns = [
     # path('polls/', polls_list, name="polls_list"),
     path('polls/', PollList.as_view(), name="polls_list"),
 
     # path('polls/<int:pk>/', polls_detail, name="polls_detail"),
-    path('polls/<int:pk>/', PollDetail.as_view(), name="polls_detail"),
+    path('polls/<int:pk>/', PollDetail.as_view(),
+         name="polls_detail"),
 
-    path('choices/', ChoiceList.as_view(), name="choice_list"),
-    path('vote/', CreateVote.as_view(), name="create_vote"),
+    path('polls/<int:pk>/choices/',
+         ChoiceList.as_view(), name="choice_list"),
+
+    path('polls/<int:pk>/choices/<int:choice_pk>/vote/',
+         CreateVote.as_view(), name="create_vote"),
 ]
+
+urlpatterns += router.urls
